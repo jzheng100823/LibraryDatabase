df431e05e94c40d8881a4f19c073c90b920ce828
Isaac <Isaac.scott.fowler@gmail.com>
2021-12-03 14:37:38 -0500

    enable search by author first + last name

--- search.php
+++ search.php
@@ -9,8 +9,10 @@
     if(isset($_POST['submit-search'])){
         $search = mysqli_real_escape_string($conn, $_POST['search']);
         //$sql = "SELECT * FROM book WHERE book.title LIKE '%$search%' or author.first_name LIKE '%$search%'or author.last_name LIKE '%$search%'";
-        $sql = "SELECT * FROM book NATURAL JOIN author WHERE book.title LIKE '%$search%' or author.first_name LIKE '%$search%' or author.last_name LIKE '%$search%'";
+        //$sql = "SELECT * FROM book NATURAL JOIN author WHERE book.title LIKE '%$search%' or author.first_name LIKE '%$search%' or author.last_name LIKE '%$search%'";
+        $sql = "SELECT * FROM book NATURAL JOIN author WHERE book.title LIKE '%$search%' OR CONCAT(author.first_name, ' ', author.last_name) LIKE '%$search%'";
         $result = mysqli_query($conn, $sql);
+        //echo "Result is: ". $result;
         $queryResult = mysqli_num_rows($result); //number of records returned
         
         echo "There are ".$queryResult." results!";
@@ -20,13 +22,13 @@
                 echo "<a href='page.php?book_id=".$row['book_id']."&publisher_id=".$row['publisher_id']."'><div class='test2'>
                     <h3>".$row['title']."</h3>
                     <p>".$row['first_name']."&nbsp;" .$row['last_name']."</p>
-                    <p>".$row['price']."<p>
+                    <p>Price: $".$row['price']."<p>
                 </div></a>";
             }
         } else{
             echo "<p style='color:red' 'font-size:20px'>There are no results matching your search!</p>";
-            echo "<button onclick=\"location.href='./'\">Search again</button>";
         }
+        echo "<button onclick=\"location.href='./'\">Search again</button>";
     }
 ?>
 </div>
